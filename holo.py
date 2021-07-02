34487d3cdb3536bb4d463d09c71145a36f69d8d5
Epihaius <Epihaius@users.noreply.github.com>
2021-07-02 18:25:37 +0000

    Update holo.py
    Clearly identify distortion camera by giving it an appropriate name.

--- holo.py
+++ holo.py
@@ -10,7 +10,7 @@ def apply_hologram(input_model, pos_adj = Vec3(0, 0, 0), scale_adj = 1):
         return base.graphicsEngine.makeOutput(base.pipe, str(in_label), -2, props, win_props,
             GraphicsPipe.BFSizeTrackHost | GraphicsPipe.BFRefuseWindow,
             base.win.get_gsg(), base.win)
-        
+
     # make the distortion buffer
     distort_buff = make_fbo("distortion_buffer")
     distort_buff.set_sort(-3)
@@ -18,7 +18,8 @@ def apply_hologram(input_model, pos_adj = Vec3(0, 0, 0), scale_adj = 1):
 
     # add a distortion camera
     distort_cam = base.makeCamera(distort_buff, scene=render, lens=base.cam.node().get_lens(), mask=BitMask32.bit(4))
-        
+    distort_cam.name = "distort_cam"
+
     # the model to be distorted
     input_model.set_pos(pos_adj)
     input_model.set_scale(scale_adj)
@@ -29,7 +30,7 @@ def apply_hologram(input_model, pos_adj = Vec3(0, 0, 0), scale_adj = 1):
     input_model.set_shader(distort_shader)
     input_model.hide(BitMask32.bit(4))
     input_model.set_transparency(TransparencyAttrib.M_dual)
-    
+
     amb_light = AmbientLight('amblight')
     amb_light.set_color((1, 1, 1, 1))
     amb_light_node = input_model.attach_new_node(amb_light)
@@ -43,7 +44,7 @@ def apply_hologram(input_model, pos_adj = Vec3(0, 0, 0), scale_adj = 1):
     distort_buff.add_render_texture(tex_distort, GraphicsOutput.RTMBindOrCopy, GraphicsOutput.RTPColor)
     input_model.set_shader_input("screen", tex_distort)
     # distortion setup ends
-        
+
 def make_wire(wire_model, pos_adj = Vec3(0, 0, 0), scale_adj = 1):
     # wireframe model
     wire_model.set_pos(pos_adj)
