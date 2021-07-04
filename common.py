6ff14f1e44856c0193a717806a0f762f1df39689
Epihaius <Epihaius@users.noreply.github.com>
2021-07-04 18:09:30 +0000

    Update common.py
    Apply `scene_shader` in Game.py instead of common.py

--- common.py
+++ common.py
@@ -25,7 +25,7 @@ base = ShowBase()
 vert_shader = "Assets/Section1/shaders/simplepbr_vert_mod_1.vert"
 frag_shader = "Assets/Section1/shaders/simplepbr_frag_mod_1.frag"
 scene_shader = Shader.load(Shader.SL_GLSL, vert_shader, frag_shader)
-base.render.set_shader(scene_shader)
+#base.render.set_shader(scene_shader)
 
 gameController = None
 
